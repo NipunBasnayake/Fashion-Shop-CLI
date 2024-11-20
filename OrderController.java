6539b56d225874cca6f9c5fccbc73ecfa3692cbb
Nipun Sathsara Basnayake <nipunsathsara1999@gmail.com>
2024-11-20 22:09:10 +0530

    Searchs implemented using Controller

--- OrderController.java
+++ OrderController.java
@@ -1,14 +1,76 @@
 import java.io.*;
+import java.util.*;
 
 public class OrderController {
 
     public static boolean placeOrder(Order newOrder) throws IOException {
-        if (newOrder!=null) {
+        if (newOrder != null) {
             FileWriter fw = new FileWriter("OrdersDoc.txt", true);
             fw.write(newOrder.toString() + "\n");
             fw.close();
-            return true;    
+            return true;
         }
         return false;
     }
+
+    public static List searchCustomerID(String customerId) throws IOException {
+        List newList = new List();
+        Scanner input = new Scanner(new File("OrdersDoc.txt"));
+        while (input.hasNext()) {
+            String line = input.nextLine();
+            String[] rowData = line.split(",");
+
+            Order newOrder = new Order(rowData[0], rowData[1], Integer.parseInt(rowData[2]),
+                    Double.parseDouble(rowData[3]), rowData[4], rowData[5]);
+
+            if (newOrder.getCustomerID().equals(customerId)) {
+                newList.add(newOrder);
+                return newList;
+            }
+        }
+        return null;
+    }
+
+    public static Order searchOrderId(String orderId) throws IOException {
+        String newLine = null;
+        BufferedReader br = new BufferedReader(new FileReader("OrdersDoc.txt"));
+        String line = br.readLine();
+
+        while (line != null) {
+            String orderID = line.substring(0, 9);
+            if (orderID.equalsIgnoreCase(orderId)) {
+                newLine = line;
+                break;
+            }
+            line = br.readLine();
+        }
+        if (newLine != null) {
+            String[] rowData = newLine.split(",");
+            Order searchOrder = new Order(rowData[0], rowData[1], Integer.parseInt(rowData[2]), Double.parseDouble(rowData[3]), rowData[4], rowData[5]);
+            return searchOrder;
+        } else {
+            return null;
+        }
+    }
+
+    public static List viewCustomes() throws IOException{
+        List orderList = new List();
+        Scanner input = new Scanner(new File("OrdersDoc.txt"));
+        while (input.hasNextLine()) {
+            String line = input.nextLine();
+            String[] rowData = line.split(",");
+            Order newOrder = new Order(rowData[0], rowData[1], Integer.parseInt(rowData[2]), Double.parseDouble(rowData[3]), rowData[4], rowData[5]);
+            orderList.add(newOrder);
+        }
+        return orderList;
+
+
+
+        
+    }
+
+
+
+
+
 }
--- SearchCustomerWindow.java
+++ SearchCustomerWindow.java
@@ -1,8 +1,6 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-import java.io.*;
-import java.util.Scanner;
 
 import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.DefaultTableModel;
@@ -50,58 +48,44 @@ public class SearchCustomerWindow extends JFrame {
 
         String[] columnNames = { "Size", "QTY", "Amount" };
         model = new DefaultTableModel(columnNames, 0);
-        
+
         btnSearch.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent evt) {
                 String customerId = txtCusID.getText().trim();
-                List searchCustomerList = new List();
-        
+                List searchCustomerList;
+
                 model.setRowCount(0);
-        
-                try (Scanner input = new Scanner(new File("OrdersDoc.txt"))) {
-                    while (input.hasNext()) {
-                        String line = input.nextLine();
-                        String[] rowData = line.split(",");
-        
-                        Order newOrder = new Order(rowData[0], rowData[1], Integer.parseInt(rowData[2]),
-                                Double.parseDouble(rowData[3]), rowData[4], rowData[5]);
-        
-                        if (newOrder.getCustomerID().equals(customerId)) {
-                            searchCustomerList.add(newOrder);
-                        }
-                    }
-                } catch (Exception e) {
-                    JOptionPane.showMessageDialog(SearchCustomerWindow.this,
-                            "Error reading orders file: " + e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
-                }
-        
                 String[] allSizes = { "XS", "S", "M", "L", "XL", "XXL" };
                 double totalAmount = 0;
-        
-                for (String size : allSizes) {
-                    int totalQtyForSize = 0;
-                    double totalAmountForSize = 0;
-        
-                    for (int i = 0; i < searchCustomerList.size(); i++) {
-                        Order foundOrder = searchCustomerList.get(i);
-                        if (foundOrder.getSize().equals(size)) {
-                            totalQtyForSize += foundOrder.getQuantity();
-                            totalAmountForSize += foundOrder.getAmount();
+
+                try {
+                    searchCustomerList = OrderController.searchCustomerID(customerId);
+                    if (searchCustomerList != null) {
+                        for (String size : allSizes) {
+                            int totalQtyForSize = 0;
+                            double totalAmountForSize = 0;
+
+                            for (int i = 0; i < searchCustomerList.size(); i++) {
+                                Order foundOrder = searchCustomerList.get(i);
+                                if (foundOrder.getSize().equals(size)) {
+                                    totalQtyForSize += foundOrder.getQuantity();
+                                    totalAmountForSize += foundOrder.getAmount();
+                                }
+                            }
+                            if (totalQtyForSize > 0) {
+                                model.addRow(new Object[] { size, totalQtyForSize,
+                                        String.format("%.2f", totalAmountForSize) });
+                                totalAmount += totalAmountForSize;
+                            } else {
+                                model.addRow(new Object[] { size, 0, "0.00" });
+                            }
                         }
-                    }
-        
-                    if (totalQtyForSize > 0) {
-                        model.addRow(new Object[] { size, totalQtyForSize, String.format("%.2f", totalAmountForSize) });
-                        totalAmount += totalAmountForSize;
+                        totalLabel.setText(String.format("Total: %.2f", totalAmount));
+
                     } else {
-                        model.addRow(new Object[] { size, 0, "0.00" });
+                        JOptionPane.showMessageDialog(null, "Customer not Found.!");
                     }
-                }
-                totalLabel.setText(String.format("Total: %.2f", totalAmount));
-        
-                if (searchCustomerList.size() == 0) {
-                    JOptionPane.showMessageDialog(SearchCustomerWindow.this,
-                            "Order not found for Customer ID: " + customerId, "Error", JOptionPane.ERROR_MESSAGE);
+                } catch (Exception e) {
                 }
             }
         });
